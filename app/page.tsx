108f9b804af5280c0b60456135c827ebc2c79ddc
Danijel Malinovic <danijelmalinovic2@gmail.com>
2024-01-30 18:49:05 +0100

    chore: setup testing with some code quality improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,30 @@
-import LoginButton from "@/components/login-button";
+import LoginButton from '@/components/login-button';
 import { auth } from './api/auth/[...nextauth]/auth';
-import React from "react";
-import LogoutButton from "@/components/logout-button";
+import React from 'react';
+import LogoutButton from '@/components/logout-button';
 
 export default async function Home() {
   const session = await auth();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       {!!session ? (
+        <div>
+          <p>
+            You are logged in as {session.user?.name} ({session.user?.email}).
+          </p>
           <div>
-            <p>
-              You are logged in as {session.user?.name} ({session.user?.email}).
-            </p>
-            <div>
-              <LogoutButton />
-            </div>
+            <LogoutButton />
           </div>
+        </div>
       ) : (
+        <div>
+          <p>You are not logged in.</p>
           <div>
-            <p>You are not logged in.</p>
-            <div>
-              <LoginButton />
-            </div>
+            <LoginButton />
           </div>
+        </div>
       )}
     </main>
-  )
+  );
 }
