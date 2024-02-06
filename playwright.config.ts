0ab598c7ee6325a37e9c69dd1a496a85e4e99a87
Danijel Malinovic <danijelmalinovic2@gmail.com>
2024-02-06 23:25:47 +0100

    chore: refactor code

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,8 +70,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: 'npm run test-mocks',
     url: 'http://127.0.0.1:3000',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !process.env.CI
   },
 });
