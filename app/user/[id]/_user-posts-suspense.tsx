aba2f9f4aae7276f9d0536ce355e6887a8bfcc8a
Danijel Malinovic <danijelmalinovic2@gmail.com>
2024-04-03 23:13:13 +0200

    feat: add suspense for user posts page

--- app/user/[id]/_user-posts-suspense.tsx
+++ app/user/[id]/_user-posts-suspense.tsx
@@ -6,28 +6,22 @@ import { UserDashboardPosts } from './_user-posts';
 import { Suspense } from 'react';
 import PostSkeleton from '@/components/skeleton/post-skeleton';
 import PostSkeletonList from '@/components/skeleton/post-skeleton-list';
-import UserPostsSuspense from './_user-posts-suspense';
+import { User } from '@/utils/models';
 
 let fakeSrc =
   'https://storage.googleapis.com/mumble-api-data/55068752-3e6d-41d4-94d8-905edc23f0a5';
 
-export default async function Home({ params }: { params: { id: string } }) {
-  const session = await auth();
-  const user = await GetUserById(params.id);
-
-  const isPersonalUser = session?.user?.id === user.id;
+export default async function UserPostsSuspense({ isPersonalUser, user }: { isPersonalUser: boolean, user: User }) {
+  const postsResponse = await GetPosts({ creators: [user.id] });
 
   return (
     <div>
-      <div className='pt-m'></div>
-
-      <Profile user={user} editable={true} imgUrl={fakeSrc}></Profile>
-
-      <div className='pt-l'></div>
-
-      <Suspense fallback={<PostSkeletonList count={10} />}>
-        <UserPostsSuspense isPersonalUser={isPersonalUser} user={user} />
-      </Suspense>
+      <UserDashboardPosts
+        userId={user.id}
+        isPersonalUser={isPersonalUser}
+        postsPaginatedResult={postsResponse}
+        queryParams={{ creators: [user.id] }}
+      ></UserDashboardPosts>
     </div>
   );
 }
