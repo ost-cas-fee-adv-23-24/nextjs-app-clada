a8752de3a6abaccd41edcec4bd9e75ab7ebf034a
Danijel Malinovic <35777482+malinovic@users.noreply.github.com>
2024-04-09 19:28:45 +0200

    fea: update meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,24 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: 'Mumble',
   description: 'Mumble Web App 2024',
+  applicationName: 'Mumble',
+  authors: [
+    {
+      url: 'https://github.com/claudio-OST',
+      name: 'Claudio Steffen'
+    },
+    {
+      url: 'https://github.com/malinovic',
+      name: 'Danijel Malinovic'
+    }
+  ],
+  keywords: [
+    'Mumble',
+    'Platform',
+    'Team Clada',
+    'Twitter / X Replacement',
+    'Much better than Twitter / X'
+  ]
 };
 
 export default function RootLayout({
