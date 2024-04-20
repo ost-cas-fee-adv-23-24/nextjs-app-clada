efec6230afa7aeaf04d5baa2cf6d1997bd193423
claudio-OST <144836241+claudio-OST@users.noreply.github.com>
2024-04-20 15:40:56 +0200

    feat: show friends basics

--- app/user/[id]/_user-posts.tsx
+++ app/user/[id]/_user-posts.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { GetPosts, GetPostsParams } from '@/app/api/actions/post.actions';
+import { GetUserFollowees, GetUsers } from '@/app/api/actions/user.actions';
 import PostList from '@/components/post-list/post-list';
+import { RecommendedUsers } from '@/components/user/recommended-users';
 import { Config } from '@/config/env';
-import { PostPaginatedResult } from '@/utils/models';
+import { PostPaginatedResult, User } from '@/utils/models';
 import { Tabs } from 'clada-storybook';
 import { useState } from 'react';
 import { UserPageSection } from './const';
@@ -28,25 +30,56 @@ export const UserDashboardPosts = ({
   const [currentQueryParams, setCurrentQueryParams] =
     useState<QueryParam>(queryParams);
 
+  const [showFriends, setShowFriends] = useState(false);
+  const [users, setUsers] = useState<Array<User>>([]);
+  const [followees, setFollowees] = useState<Array<User>>([]);
+
   const onSectionChange = async (section: UserPageSection) => {
     const temporaryQueryParam: QueryParam = {};
+
     switch (section) {
-      case 'liked':
-        temporaryQueryParam.likedBy = [userId];
-        break;
-      case 'created':
+      case UserPageSection.CreatedByUser:
         temporaryQueryParam.creators = [userId];
+        handleSearch(temporaryQueryParam);
+        break;
+      case UserPageSection.LikedByUser:
+        temporaryQueryParam.likedBy = [userId];
+        handleSearch(temporaryQueryParam);
+        break;
+      case UserPageSection.Friends:
+        handleFriends();
         break;
     }
+  };
 
+  const handleSearch = async (param: QueryParam) => {
     const apiPosts = await GetPosts({
       offset: 0,
       limit: Config.defaultPageSize,
-      ...temporaryQueryParam,
+      ...param,
     });
 
-    setCurrentQueryParams(temporaryQueryParam);
+    setCurrentQueryParams(param);
     setCurrentPaginatedResult(apiPosts);
+
+    if (showFriends) {
+      setShowFriends(false);
+    }
+  };
+
+  const handleFriends = async () => {
+    const loadedUsers = (await GetUsers()).data
+      .filter((x) => x.id !== userId)
+      .slice(-6);
+
+    setUsers(loadedUsers as Array<User>);
+
+    const loadedFollowees = userId ? (await GetUserFollowees(userId)).data : [];
+    setFollowees(loadedFollowees);
+
+    if (!showFriends) {
+      setShowFriends(true);
+    }
   };
 
   return (
@@ -63,15 +96,30 @@ export const UserDashboardPosts = ({
                 onClick: () => onSectionChange(UserPageSection.LikedByUser),
                 text: 'Deine Likes',
               },
+              {
+                onClick: () => onSectionChange(UserPageSection.Friends),
+                text: 'Deine Freunde',
+              },
             ]}
           />
           <div className='pt-l'></div>
         </>
       )}
-      <PostList
-        postsPaginatedResult={currentPaginatedResult}
-        queryParams={currentQueryParams}
-      />
+      {isPersonalUser && showFriends ? (
+        <div>
+          <div className='pt-s'></div>
+          <RecommendedUsers
+            users={users}
+            followees={followees}
+          ></RecommendedUsers>
+          <div className='pt-s'></div>
+        </div>
+      ) : (
+        <PostList
+          postsPaginatedResult={currentPaginatedResult}
+          queryParams={currentQueryParams}
+        />
+      )}
     </>
   );
 };
