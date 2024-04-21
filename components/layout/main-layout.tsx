2edd951525d555558ea1f8971c81bec637d10d59
Danijel Malinovic <35777482+malinovic@users.noreply.github.com>
2024-04-21 23:09:54 +0200

    fix: update

--- components/layout/main-layout.tsx
+++ components/layout/main-layout.tsx
@@ -2,11 +2,13 @@ import { SessionProvider } from 'next-auth/react';
 import { ReactNode } from 'react';
 import { Footer } from './footer';
 import { Header } from './header';
+import { ServiceWorker } from '@/components/layout/service-worker';
 
 export const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
     <SessionProvider>
       <Header />
+      <ServiceWorker />
       <div className='pt-l'></div>
       <main className='grid w-full min-h-screen place-items-center items-start bg-base-100 py-xl pl-l pr-l pt-header'>
         <div className='flex max-w-[680px] w-full'>{children}</div>
