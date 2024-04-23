cf2a90e6a4480cb14849f6fcb1ba408f29cc028b
Danijel Malinovic <danijel.malinovic@emea.merkleinc.com>
2024-04-23 21:23:00 +0200

    change retries to 1

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   testDir: './__tests__',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
